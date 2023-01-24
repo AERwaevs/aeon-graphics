d369eb26e8b916f8ef45f98a71abaf1cf49c47fe
Hammy Cooper <thehamstarr@gmail.com>
2023-01-24 21:39:48 +0800

    add pos_x and pos_y to WindowProperties

--- Window.h
+++ Window.h
@@ -1,54 +1,55 @@
 #pragma once
 
-#include <Graphics/Renderer.h>
-
-class Event;
-class Events;
-
 namespace AEON
 {
-    struct AEON_DLL WindowProperties
-    {
-        String      m_name;
-        bool        m_fullscreen;
-        uint32_t    m_width;
-        uint32_t    m_height;
 
-        WindowProperties( const String&     name        = "AEON",
-                                bool        fullscreen  = false,
-                                uint32_t    width       = 1280,
-                                uint32_t    height      = 720 ) 
-        :   m_name(       name ),
-            m_fullscreen( fullscreen ),
-            m_width(      width ),
-            m_height(     height ) 
-        {};
-    };
+class Graphics::Renderer;
 
-    class AEON_DLL Window : public Inherit< Object, Window >
-    {
-        using Properties = WindowProperties;
-        using Renderer   = Graphics::Renderer;
+struct AEON_DLL WindowProperties
+{
+    String      m_name;
+    bool        m_fullscreen;
+    uint32_t    m_width;
+    uint32_t    m_height;
+    uint32_t    m_pos_x;
+    uint32_t    m_pos_y;
 
-    public:
-        static  Shared<Window>      create( const Properties& props = Properties() );
-        
-        virtual uint32_t            GetWidth()          const           = 0;
-        virtual uint32_t            GetHeight()         const           = 0;
-        virtual String              GetName()           const           = 0;
-        virtual bool                GetVsync()          const           = 0;
+    WindowProperties( const String&     name        = "AEON",
+                            bool        fullscreen  = false,
+                            uint32_t    width       = 1280,
+                            uint32_t    height      = 720,
+                            uint32_t    pos_x       = 0,
+                            uint32_t    pos_y       = 0 ) 
+    :   m_name( name ), m_fullscreen( fullscreen ), m_width( width ),
+        m_height( height ), m_pos_x( pos_x ), m_pos_y( pos_y )
+    {};
+};
 
-        virtual void                SetName( const String&  name )      = 0;
-        virtual void                SetVsync(      bool     enabled )   = 0;
+class AEON_DLL Window : public Inherit< Object, Window >
+{
+    using Properties = WindowProperties;
+    using Renderer = Graphics::Renderer;
+
+public:
+    static  Shared<Window>      create( const Properties& props = Properties() );
     
-        virtual bool                PollEvents( Events& events_list )   = 0;
-    protected:
-        virtual                     ~Window() = default;
+    virtual uint32_t            GetWidth()          const           = 0;
+    virtual uint32_t            GetHeight()         const           = 0;
+    virtual String              GetName()           const           = 0;
+    virtual bool                GetVsync()          const           = 0;
 
-    protected:
-                Events              m_events_buffer;
-                Shared<Renderer>    m_renderer;
+    virtual void                SetName( const String&  name )      = 0;
+    virtual void                SetVsync(      bool     enabled )   = 0;
 
-    };
-    using Windows = List<Shared<Window>>;
+    virtual bool                PollEvents( Events& events_list )   = 0;
+protected:
+    virtual                     ~Window() = default;
+
+protected:
+            Events              m_events_buffer;
+            Shared<Renderer>    m_renderer;
+
+};
+using Windows = List<Shared<Window>>;
+
 }

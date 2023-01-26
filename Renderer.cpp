93748d90acf2c1933c715e3b5056585f67f7bddc
Hammy Cooper <thehamstarr@gmail.com>
2023-01-26 22:58:19 +0800

    tidy renderer.h, add s_instance

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,10 +1,34 @@
 #include <Graphics/Renderer.h>
 
+#include <Vendor/glad/include/glad/gl.h>
+#include <Vendor/glad/include/glad/vulkan.h>
+
+#define AEON_GL_VERSION GLAD_MAKE_VERSION( 4, 6 )
+#define AEON_VK_VERSION GLAD_MAKE_VERSION( 1, 2 )
+
 #include <Platform/Vulkan/VulkanRenderer.h>
 #include <Platform/OpenGL/OpenGLRenderer.h>
 
 namespace AEON::Graphics
 {    
+    static bool OpenGLSupported()
+    {
+        int version = gladLoaderLoadGL();  //! returns 0, can't load opengl?
+        AE_INFO( "OpenGL version: %d.%d", 
+                    GLAD_VERSION_MAJOR(version),
+                    GLAD_VERSION_MINOR(version) );
+        return version >= AEON_GL_VERSION;
+    }
+
+    static bool VulkanSupported()
+    {
+        int version = gladLoaderLoadVulkan( nullptr, nullptr, nullptr );
+        AE_INFO( "Vulkan version: %d.%d", 
+                    GLAD_VERSION_MAJOR(version),
+                    GLAD_VERSION_MINOR(version) );
+        return version >= AEON_VK_VERSION;
+    }
+
     Shared<Renderer> Renderer::create( GraphicsAPI api )
     {
         if( api == Vulkan && VulkanSupported() )
--- Renderer.h
+++ Renderer.h
@@ -1,46 +1,26 @@
 #pragma once
 
-#include <Vendor/glad/include/glad/gl.h>
-#include <Vendor/glad/include/glad/vulkan.h>
-
-#define AEON_GL_VERSION GLAD_MAKE_VERSION( 4, 6 )
-#define AEON_VK_VERSION GLAD_MAKE_VERSION( 1, 2 )
-
 namespace AEON::Graphics
 {
-    class AEON_DLL Renderer : public Inherit< Object, Renderer >
-    {
-        public:
-            typedef enum : uint8_t
-            {
-                None    = 0,
-                Vulkan  = BIT(0),
-                OpenGL  = BIT(1)
-            } GraphicsAPI;
 
-            static Shared<Renderer> create( GraphicsAPI api = GraphicsAPI::None );
+class AEON_DLL Renderer : public Inherit< Object, Renderer >
+{
+    public:
+        typedef enum : uint8_t
+        {
+            None    = 0,
+            Vulkan  = BIT(0),
+            OpenGL  = BIT(1)
+        } GraphicsAPI;
 
-            static bool OpenGLSupported()
-            {
-                int version = gladLoaderLoadGL();  //! returns 0, can't load opengl?
-                AE_INFO( "OpenGL version: %d.%d", 
-                         GLAD_VERSION_MAJOR(version),
-                         GLAD_VERSION_MINOR(version) );
-                return version >= AEON_GL_VERSION;
-            }
+        static Shared<Renderer> create( GraphicsAPI api = GraphicsAPI::None );
+        static Shared<Renderer> instance() { return s_instance; }
 
-            static bool VulkanSupported()
-            {
-                int version = gladLoaderLoadVulkan( nullptr, nullptr, nullptr );
-                AE_INFO( "Vulkan version: %d.%d", 
-                         GLAD_VERSION_MAJOR(version),
-                         GLAD_VERSION_MINOR(version) );
-                return version >= AEON_VK_VERSION;
-            }
+    protected:
+        virtual ~Renderer() = default;
+    private:
+        static inline   Shared<Renderer> s_instance;
+        static inline   GraphicsAPI      m_graphics_api = None;
+};
 
-        protected:
-            virtual ~Renderer() = default;
-        private:
-            static inline GraphicsAPI m_graphics_api = None;
-    };
 }

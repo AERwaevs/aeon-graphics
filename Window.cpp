94adc2d2d6d24d1ecda0ed501f39ff0f3772f305
Hammy Cooper <thehamstarr@gmail.com>
2023-03-22 17:26:43 +0800

    move window to graphics

---
+++ Window.cpp
@@ -0,0 +1,16 @@
+#include <Graphics/Window.h>
+
+namespace AEON::Graphics
+{
+    
+void AEON_API Window::Update()
+{
+    // TODO implement viewport functions
+    //while( m_viewport->AdvanceFrame() )
+    //{
+    //    m_viewport->Update();
+    //    m_viewport->Present();
+    //}
+}
+    
+} // namespace AEON::Graphics

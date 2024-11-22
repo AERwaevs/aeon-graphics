7144a5ba6d9eb87033bbcef0c039d9de384903e2
AERwaevs <thehamstarr@gmail.com>
2024-11-22 22:50:00 +0800

    change window events to store const data, rename resize to configure

--- inc/Events/WindowEvents.h
+++ inc/Events/WindowEvents.h
@@ -10,40 +10,25 @@ namespace aer
 
 struct WindowEvent : public Event
 {
-    WindowEvent( Window* window ) : _window( window ) {};
-    
-    spy_ptr<Window>  window() const { return _window; }
-private:
-    spy_ptr<Window>  _window;
+    WindowEvent( Window* in_window ) : window( in_window ) {};
+    const spy_ptr<Window>  window;
 };
 
 struct WindowCloseEvent : public WindowEvent
 {
-    WindowCloseEvent( auto* window ) : WindowEvent( window ) {};
+    WindowCloseEvent( auto window ) : WindowEvent( window ) {};
+};
+
+struct WindowExposeEvent : public WindowEvent
+{
+    WindowExposeEvent( auto window ) : WindowEvent( window ) {};
 };
         
-struct WindowMoveEvent : public WindowEvent
+struct WindowConfigureEvent : public WindowEvent
 {
-    WindowMoveEvent( auto* window, uint32_t pos_x, uint32_t pos_y )
-    : WindowEvent( window ), _x( pos_x ), _y( pos_y ) {};
-    
-    uint32_t x() const { return _x; }
-    uint32_t y() const { return _y; }
-private:
-    uint32_t _x;
-    uint32_t _y;
-};
-
-struct WindowResizeEvent : public WindowEvent
-{
-    WindowResizeEvent( auto* window, uint32_t width, uint32_t height )
-    : WindowEvent( window ), _width( width ), _height( height ) {};
-
-    uint32_t    width()  const { return _width; }
-    uint32_t    height() const { return _height; }
-private:
-    uint32_t    _width;
-    uint32_t    _height;
+    WindowConfigureEvent( auto window, uint32_t pos_x, uint32_t pos_y, uint32_t width, uint32_t height )
+    : WindowEvent( window ), x( pos_x ), y( pos_y ), width( width ), height( height ) {};
+    const uint32_t x, y, width, height;
 };
 
 struct WindowMinimizeEvent : public WindowEvent
